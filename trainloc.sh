c4eb41b37320af9c6a53a642a1562d71efda90f7
xinyuanqian <qianxinyuan@pbcmlg01.cm.cluster>
2024-02-26 11:15:58 +0800

    updates

---
+++ trainloc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -J IRcls
+#SBATCH -N 1
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=1
+#SBATCH -p p-RTX2080
+#SBATCH -A t00120220002
+
+source /cm/shared/apps/anaconda3/etc/profile.d/conda.sh
+conda activate image2rev
+
+python main_sslr.py -model 'MLP3'
